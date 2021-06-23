53efa3a4ddd9d77353dce165e171898e65b7effe
naseo <nasarisk9204@gmail.com>
2021-06-23 23:00:42 +0900

    Create login form ETC

--- routes/login.js
+++ routes/login.js
@@ -11,4 +11,13 @@ router.get('/', (req,res) =>{
     res.render('login/login');
 });
 
+router.post('/', (req,res) =>{
+    let email = req.body.email;
+    let password = req.body.password;
+    if(email === "admin@naseo.com" && password === "admin1234"){
+        res.render('home/welcome');    
+    }
+    res.render('login/login');
+});
+
 module.exports = router;
